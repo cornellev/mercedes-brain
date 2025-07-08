1290d38a9947637a3e0bf94035b054623fce8b96
Erica Jiang <115647009+yuchenie@users.noreply.github.com>
2025-07-08 15:40:29 -0400

    Delete build directory

--- build/serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erica/mercedes-brain/src/serial-ros2
-# Build directory: /home/erica/mercedes-brain/build/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

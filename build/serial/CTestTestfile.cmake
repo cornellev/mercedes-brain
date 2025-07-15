934dbf0bbeb0e8e5ef1d9b29a9fd60ec34aa6aef
Erica Jiang <ericaijiang@gmail.com>
2025-07-15 19:57:34 -0400

    steer and drive (without steer sensor)

---
+++ build/serial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/erica/mercedes-brain/src/serial-ros2
+# Build directory: /home/erica/mercedes-brain/build/serial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/serial_com/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/erica/mercedes-brain/install/serial/share/serial/package.sh"
